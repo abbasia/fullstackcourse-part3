0563d234d70359230db768a5a3b28dc32512ad39
abbasi <abbasi@abbasis-MBP.home>
2019-10-20 20:20:14 +0300

    build folder added

---
+++ build/precache-manifest.6d83405549dbad44a0f016f9f7b39354.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a421ff1e84ea9b6f051a93599746f89d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bf3186ffe63c50c3bb9",
+    "url": "/static/css/main.265e57ff.chunk.css"
+  },
+  {
+    "revision": "63d2c87dedef8886d2b2",
+    "url": "/static/js/2.16d4fb6c.chunk.js"
+  },
+  {
+    "revision": "9bf3186ffe63c50c3bb9",
+    "url": "/static/js/main.921692cd.chunk.js"
+  },
+  {
+    "revision": "d92e7acfcbfa197868a5",
+    "url": "/static/js/runtime-main.d36c9b34.js"
+  }
+]);
